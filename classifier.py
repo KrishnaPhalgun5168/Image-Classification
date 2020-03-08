072346ea73c6b3f519509487a34e432e526e3a11
KrishnaPhalgun5168 <59023719+KrishnaPhalgun5168@users.noreply.github.com>
2020-03-08 18:44:35 +0530

    Add files via upload

---
+++ classifier.py
@@ -0,0 +1,16 @@
+import numpy as np
+from keras.preprocessing import image
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+
+classifier = ResNet50(weights='imagenet')
+
+def classify(path=None):
+	if path:
+		img = image.img_to_array(image.load_img(path, target_size=(224, 224)))
+		img = preprocess_input(np.expand_dims(img, axis=0))
+		predictions = np.squeeze(decode_predictions(classifier.predict(img)), axis=0)
+		for label in predictions:
+			print("prediction: {} with confidence: {}\n".format(label[1], label[2]))
+
+classify('fountainpen.jpg')
+#classify('keyboardmouse.jpg')
